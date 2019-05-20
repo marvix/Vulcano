ca75093886139764c506da4c4b74063a9ce5f3aa
Fernando Salles Claro <fsclaro@gmail.com>
2019-05-20 11:53:47 -0300

    criação do seed de usuários

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // cria um usuário administrador
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin1234'),
+            'active' => true,
+            'super_admin' => true,
+            'gender' => 'N',
+            'avatar' => null,
+            'remember_token' => str_random(10),
+            'created_at' => now()
+        ]);
+    }
+}
