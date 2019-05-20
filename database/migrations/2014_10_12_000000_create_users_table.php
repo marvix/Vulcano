0d262677b0147a8f0a2986f179ed079b0a0c0a46
Fernando Salles Claro <fsclaro@gmail.com>
2019-05-20 11:53:33 -0300

    melhorias na tabela de usuários

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->boolean('active')->default(true);
+            $table->boolean('super_admin')->default(false);
+            $table->string('gender')->default('N');
+            $table->string('avatar')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
