bb8c1193774f9acae3d2122112fb32a0602be6c7
Fernando Salles Claro <fsclaro@gmail.com>
2019-05-25 19:10:56 -0300

    adição de rotas de profile e usuários

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,34 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+ * ----------------------------------------------------------------------------
+ * Rotas para edição do profile do usuário
+ * ----------------------------------------------------------------------------
+ */
+Route::get('/profile/edit', 'UsersController@editProfile')->name('profile.edit');
+Route::put('/profile/{id}', 'UsersController@updateProfile')->name('profile.update');
+
+/*
+ * ----------------------------------------------------------------------------
+ * Rotas para a tabela de usuários
+ * ----------------------------------------------------------------------------
+ */
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{id}', 'UsersController@show')->name('users.show');
+Route::get('/users/{id}/edit', 'UsersController@edit')->name('users.edit');
+Route::put('/users/{id}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{id}/delete', 'UsersController@destroy')->name('users.destroy');
+Route::get('/users/deleteavataruser/{id}', 'UsersController@deleteAvatarUser')->name('delete.avatar.user');
+Route::get('/users/active/{id}', 'UsersController@activeUser')->name('users.active');
+Route::get('/users/desactive/{id}', 'UsersController@desactiveUser')->name('users.desactive');
+
+/*
+ * ----------------------------------------------------------------------------
+ * Rotas para tratamento dos avatares dos usuários
+ * ----------------------------------------------------------------------------
+ */
+Route::get('/profile/deleteAvatarProfile/', 'UsersController@deleteAvatarProfile')->name('delete.avatar.profile');
