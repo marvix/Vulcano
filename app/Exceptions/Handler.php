843ff678ec2e9ba80aeb34eb78d90b2fa3a3cce6
Fernando Salles Claro <fsclaro@gmail.com>
2019-05-25 19:09:01 -0300

    tratamento de exceções

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileUnacceptableForCollection;
+use Spatie\MediaLibrary\Exceptions\FileCannotBeAdded;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        // Se o tipo de arquivo não for permitido...
+        if ($exception instanceOf FileUnacceptableForCollection) {
+            return redirect()
+                ->back()
+                ->with('message', 'Somente imagens JPEG, JPG ou PNG são permitidas.')
+                ->with('type', 'danger');
+        }
+        // Se a imagem não pode ser carregada ...
+        if ($exception instanceOf FileCannotBeAdded) {
+            return redirect()
+                ->back()
+                ->with('message', 'Algo deu errado. A imagem não pode ser selecionada. Tente novamente.')
+                ->with('type', 'danger');
+        }
+
         return parent::render($request, $exception);
     }
 }
