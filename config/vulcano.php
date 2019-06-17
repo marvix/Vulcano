2796c5a47dd592c164e428abfd8adf9b122c9d0c
Fernando Salles Claro <fsclaro@gmail.com>
2019-06-17 00:00:30 -0300

    configuration

---
+++ config/vulcano.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /**
+     * Brazilian date format
+     */
+    $date_format = "d/m/Y",
+    $date_format_js = "d/m/Y",
+
+    /**
+     * Brazilian timestamp format
+     */
+    $timestamp_format = "d/m/Y H:i",
+    $timestamp_format = "d/m/Y H:i"
+];
