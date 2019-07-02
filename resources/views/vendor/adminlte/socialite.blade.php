e261fffd2a0cb801f803166c7fe0bf64498afd13
Fernando Salles Claro <fsclaro@gmail.com>
2019-07-02 10:15:43 -0300

    correção no código

--- resources/views/vendor/adminlte/socialite.blade.php
+++ resources/views/vendor/adminlte/socialite.blade.php
@@ -1,18 +1,18 @@
 <p class="text-center">
     @if(env('FACEBOOK_ENABLE'))
-    <a href="#" class="btn btn-facebook btn-flat"><i class="fa fa-facebook"></i></i></a>
+    <a href="#" class="btn btn-facebook btn-flat"><i class="fa fa-facebook"></i></a>
     @endif
 
     @if(env('TWITTER_ENABLE'))
-    <a href="#" class="btn btn-twitter btn-flat"><i class="fa fa-twitter"></i></i></a>
+    <a href="#" class="btn btn-twitter btn-flat"><i class="fa fa-twitter"></i></a>
     @endif
 
     @if(env('LINKEDIN_ENABLE'))
-    <a href="#" class="btn btn-linkedin btn-flat"><i class="fa fa-linkedin"></i></i></a>
+    <a href="#" class="btn btn-linkedin btn-flat"><i class="fa fa-linkedin"></i></a>
     @endif
 
     @if(env('GOOGLE_ENABLE'))
-    <a href="#" class="btn btn-google btn-flat"><i class="fa fa-google"></i></i></a>
+    <a href="#" class="btn btn-google btn-flat"><i class="fa fa-google"></i></a>
     @endif
 
 </p>
